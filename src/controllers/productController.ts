ee9c0c1438468f39ba229751fd15e10e0aff440d
Cem Tanrikut <cemtanrikut@gmail.com>
2024-06-11 22:23:48 +0200

    create add product prop controller

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -10,3 +10,14 @@ export const createProduct = (req: Request, res: Response): void => {
     res.status(400).send({ error: error.message });
   }
 };
+
+export const addProductProperty = (req: Request, res: Response): void => {
+    const { sku } = req.params;
+    const { propertyName, options } = req.body;
+    try {
+      productService.addProductProperty(sku, propertyName, options);
+      res.status(200).send({ message: 'Property added successfully' });
+    } catch (error) {
+      res.status(400).send({ error: error.message });
+    }
+  };
