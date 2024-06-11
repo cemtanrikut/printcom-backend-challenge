611f8e5127285f4fde2feaf5382e9c83a60f5e54
Cem Tanrikut <cemtanrikut@gmail.com>
2024-06-11 22:11:50 +0200

    add create product service

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -3,5 +3,15 @@ import { Product, ProductConfiguration, ProductProperty } from '../models/produc
 export class ProductService {
     private products: { [sku: string]: Product } = {};
 
-
+    // create product service
+    createProduct(sku: string): void {
+        if (this.products[sku]) {
+          throw new Error(`Product with SKU ${sku} already exists.`);
+        }
+        this.products[sku] = {
+          sku,
+          properties: [],
+          configurations: [],
+        };
+      }
 }
