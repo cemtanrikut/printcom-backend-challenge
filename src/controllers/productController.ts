20a3d967fffed4bab74c8c72835ef0a91e5f55a8
Cem Tanrikut <cemtanrikut@gmail.com>
2024-06-11 22:25:48 +0200

    add set product config controller

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -2,13 +2,13 @@ import { Request, Response } from 'express';
 import productService from '../services/productService';
 
 export const createProduct = (req: Request, res: Response): void => {
-  const { sku } = req.body;
-  try {
-    productService.createProduct(sku);
-    res.status(201).send({ message: 'Product created successfully' });
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
+    const { sku } = req.body;
+    try {
+        productService.createProduct(sku);
+        res.status(201).send({ message: 'Product created successfully' });
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 };
 
 export const addProductProperty = (req: Request, res: Response): void => {
@@ -20,4 +20,15 @@ export const addProductProperty = (req: Request, res: Response): void => {
     } catch (error) {
       res.status(400).send({ error: error.message });
     }
-  };
+};
+
+export const setProductConfiguration = (req: Request, res: Response): void => {
+    const { sku } = req.params;
+    const { propertyValues, purchaseCost, margin } = req.body;
+    try {
+      productService.setProductConfiguration(sku, propertyValues, purchaseCost, margin);
+      res.status(200).send({ message: 'Configuration set successfully' });
+    } catch (error) {
+      res.status(400).send({ error: error.message });
+    }
+};
